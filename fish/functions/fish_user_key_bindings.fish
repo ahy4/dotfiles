23cf1090aca56307323a973135c51363609c38a5
ahyahya1 <mrfool38@gmail.com>
2019-07-28 21:22:24 +0900

    feat: add ^$ keybind for fish

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,8 @@
 # my keybindings
 function fish_user_key_bindings
   bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+  bind H "beginning-of-line"
+  bind L "end-of-line"
 end
 
 
