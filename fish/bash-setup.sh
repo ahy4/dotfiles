886fc9abfe0cb451da384397d3b2527a638f716b
ahyahya1 <mrfool38@gmail.com>
2019-10-22 23:17:59 +0900

    update path

--- fish/bash-setup.sh
+++ fish/bash-setup.sh
@@ -12,3 +12,4 @@ export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g "" | cat'
 
 export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+stack --version >/dev/null 2>&1 && export PATH=$(stack path --compiler-bin):$PATH &
