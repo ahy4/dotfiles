e437311ca1b45cc7a01e3479a7504c752411ea67
ahy <mrfool38@gmail.com>
2020-11-07 09:17:42 +0900

    fix: ansible

--- fish/bash-setup.sh
+++ fish/bash-setup.sh
@@ -15,4 +15,3 @@ export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PR
 export GOPATH=$HOME/go
 
 export PATH="$PATH:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$GOPATH/bin"
-stack --version >/dev/null 2>&1 && export PATH=$(stack path --compiler-bin):$PATH &
